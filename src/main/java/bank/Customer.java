06dc0b917712278f51f6afc973c2e6ba2625de88
c-shawn710 <c_shawn710@yahoo.com>
2025-02-22 06:58:06 +0000

    menu

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -7,6 +7,7 @@ public class Customer {
   private String username;
   private String password;
   private int accountId;
+  private boolean authenticated;
 
 
   public Customer(int id, String name, String username, String password, int accountId) {
@@ -55,6 +56,15 @@ public class Customer {
 
   public void setAccountId(int accountId) {
     this.accountId = accountId;
+  }
+
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
   }
 
 }
